daaf325dc7494c2e49407077c7a41db23ca1dfe8
Aditya Kumar <aditya@Adityas-Mac-mini.local>
2017-03-21 15:31:51 +0530

    added testVC

---
+++ LoginPage/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  LoginPage
+//
+//  Created by Aditya Kumar on 21/03/17.
+//  Copyright © 2017 mindfire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
